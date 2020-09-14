c7810ee3cbc843900a0aeafa7ea3600390cf98ec
Christian Fleury <chfleurysiq@gmail.com>
2020-09-14 14:27:43 -0300

    minor changes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -39,90 +39,93 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          'Foodgram',
-          style: TextStyle(fontFamily: 'SIMPLIFICA Typeface', fontSize: 50),
-        ),
-        Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 30,
-                  vertical: 10,
-                ),
-                child: TextFormField(
-                  validator: (input) => (input.length > 5 &&
-                          input.trim().length > 0 &&
-                          input.contains('@'))
-                      ? null
-                      : 'Por favor insira um email válido',
-                  decoration: InputDecoration(labelText: 'E-mail'),
-                  onSaved: (input) => _email = input,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 30,
-                  vertical: 10,
-                ),
-                child: TextFormField(
-                  cursorColor: Colors.pink,
-                  obscureText: true,
-                  validator: (input) => input.length < 6
-                      ? 'A senha deve conter pelo menos 6 caracteres'
-                      : null,
-                  decoration: InputDecoration(labelText: 'Senha'),
-                  onSaved: (input) => _senha = input,
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              BlocListener<LoginBloc, LoginState>(
-                bloc: _loginBloc,
-                listener: (context, state) {
-                  if (state is LoginLoaded) {
-                    Navigator.of(context).pushReplacementNamed(HomeScreen.id);
-                  } else if (state is LoginError) {
-                    
-                    print('Usuario ou senha invalidos');
-                  }
-                },
-                child: BlocBuilder<LoginBloc, LoginState>(
-                    bloc: _loginBloc,
-                    builder: (context, state) {
-                      if (state is LoginLoading) {
-                        return Center(
-                          child: LinearProgressIndicator(),
-                        );
-                      }
-                      return GradientButton(label: 'Entrar', onPressed: _submit);
-                    }),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Foodgram',
+              style: TextStyle(fontFamily: 'SIMPLIFICA Typeface', fontSize: 50),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
                 children: <Widget>[
-                  Text('Ainda não possui uma conta?'),
-                  _buildRegisterButton(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 10,
+                    ),
+                    child: TextFormField(
+                      validator: (input) => (input.length > 5 &&
+                              input.trim().length > 0 &&
+                              input.contains('@'))
+                          ? null
+                          : 'Por favor insira um email válido',
+                      decoration: InputDecoration(labelText: 'E-mail'),
+                      onSaved: (input) => _email = input,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 10,
+                    ),
+                    child: TextFormField(
+                      cursorColor: Colors.pink,
+                      obscureText: true,
+                      validator: (input) => input.length < 6
+                          ? 'A senha deve conter pelo menos 6 caracteres'
+                          : null,
+                      decoration: InputDecoration(labelText: 'Senha'),
+                      onSaved: (input) => _senha = input,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  BlocListener<LoginBloc, LoginState>(
+                    bloc: _loginBloc,
+                    listener: (context, state) {
+                      if (state is LoginLoaded) {
+                        Navigator.of(context)
+                            .pushReplacementNamed(HomeScreen.id);
+                      } else if (state is LoginError) {
+                        print('Usuario ou senha invalidos');
+                      }
+                    },
+                    child: BlocBuilder<LoginBloc, LoginState>(
+                        bloc: _loginBloc,
+                        builder: (context, state) {
+                          if (state is LoginLoading) {
+                            return Center(
+                              child: LinearProgressIndicator(),
+                            );
+                          }
+                          return GradientButton(
+                              label: 'Entrar', onPressed: _submit);
+                        }),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Ainda não possui uma conta?'),
+                      _buildRegisterButton(),
+                    ],
+                  ),
                 ],
+                mainAxisSize: MainAxisSize.min,
               ),
-            ],
-            mainAxisSize: MainAxisSize.min,
-          ),
-        )
-      ],
-    )));
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   _buildRegisterButton() {
